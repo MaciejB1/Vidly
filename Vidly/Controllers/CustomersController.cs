2feb5ef38d6f7f00df73d83eca011c05a29abd7f
Maciej Bąbol <maciekbbol@gmail.com>
2019-11-06 20:16:54 +0100

    Add ModelView for cusstomers and movies

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = new List<Customer>      //hard code data
+            {
+                new Customer() {Name = "Jan Kowalski"},
+                new Customer() {Name = "Anna Pawlik"}
+            };
+
+            var CustomersViewModel = new CustomerViewModel
+            {
+                Customers = customers
+            };
+            
+            return View(CustomersViewModel);
+        }
+    }
+}
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -3,15 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Index()
+//        public ActionResult Index(int? pageIndex, string sortBy)
+//        {
+//            return View();
+//        }
+//
+//        [Route("movies/released/{year}/{month:regex(\\d{2}):range(1, 12)}")]
+//        public ActionResult ByReleaseDate(int year, int month)
+//        {
+//            return Content(year + "/" + month);
+//        }
+
+        public ActionResult Random()
         {
-            return View();
+            var movies = new List<Movie>        //hard code data
+            {
+                new Movie() {Name = "Shrek"},
+                new Movie() {Name = "Joker"}
+            };
+
+            var moviesViewModel = new MovieViewModel
+            {
+                Movies = movies
+            };
+
+            return View(moviesViewModel);
         }
     }
 }
