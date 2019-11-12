9a1bed37375322c473c87d15112a46458bcae383
Maciej Bąbol <maciekbbol@gmail.com>
2019-11-12 22:42:56 +0100

    Small fixes

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+            
+            return View(customers);
+        }
+
+ //       [Route("Customers/Details/{id}")]
+        public ActionResult Details(int? id)
+        {
+            var customerDetails = _context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerDetails == null)
+                return HttpNotFound();
+
+            customerDetails.MembershipType = _context.MembershipTypes.SingleOrDefault(q => q.Id == customerDetails.Id);
+
+            return View(customerDetails);
+        }
+    }
+}
