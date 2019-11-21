01dbab409ed23c8d0cda5ae1e9e8258dcc3a190d
Maciej Bąbol <maciekbbol@gmail.com>
2019-11-21 17:39:50 +0100

    Building forms (#4)
    * Initial commit for new customer form
    
    * Customer form for editing and adding customers in DB
    
    * MovieFrom initial
    
    * Edit for movies
    
    * small fixes
    
    * valid

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
@@ -22,6 +23,47 @@ namespace Vidly.Controllers
             _Context.Dispose();
         }
 
+
+        public ActionResult New()
+        {
+            var membershipTypes = _Context.MembershipTypes.ToList();
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View("CustomerForm", viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Save(Customer customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                var viewModel = new CustomerFormViewModel()
+                {
+                    Customer = customer,
+                    MembershipTypes = _Context.MembershipTypes.ToList()
+                };
+                return View("CustomerForm", viewModel);
+            }
+
+            if(customer.Id == 0)
+                _Context.Customers.Add(customer);
+            else
+            {
+                var customerInDb = _Context.Customers.Single(c => c.Id == customer.Id);
+                customerInDb.Name = customer.Name;
+                customerInDb.Birthday = customer.Birthday;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+            }
+            _Context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
         // GET: Customers
         public ActionResult Index()
         {
@@ -40,6 +82,22 @@ namespace Vidly.Controllers
 
 
             return View(customerDetails);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var customer = _Context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            var viewModel = new CustomerFormViewModel()
+            {
+                Customer = customer,
+                MembershipTypes = _Context.MembershipTypes.ToList()
+            };
+
+            return View("CustomerForm", viewModel);
         }
     }
 }
--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -9,23 +9,24 @@ namespace Vidly.Models
     public class Movie
     {
         [Key]
-        [Required]
         public int Id { get; set; }
         [Required]
         [StringLength(255)]
         public string Name { get; set; }
         [Required]
         [DataType(DataType.Date)]
+        [Display(Name = "Release Date")]
         public DateTime ReleaseDate { get; set; }
         [Required]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:year-month-day}", ApplyFormatInEditMode = true)]
         public DateTime DateAdded { get; set; }
         [Required]
+        [Display(Name = "Number in Stock")]
         public byte NumberInStock { get; set; }
         [Required]
+        [Display(Name = "Genre")]
         public byte GenreId { get; set; }
-        [Required]
         public Genre Genre { get; set; }
     }
 }
