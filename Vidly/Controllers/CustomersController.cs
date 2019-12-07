af62629fbbae2552428e5fb236c33a586ee0b13d
Maciej Bąbol <maciekbbol@gmail.com>
2019-12-07 12:32:54 +0100

    Validation summary for customers

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -30,6 +30,7 @@ namespace Vidly.Controllers
 
             var viewModel = new CustomerFormViewModel()
             {
+                Customer = new Customer(),
                 MembershipTypes = membershipTypes
             };
 
