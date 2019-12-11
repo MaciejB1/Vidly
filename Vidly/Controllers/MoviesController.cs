6b2e6fd729c007b2c71fa137127a4ebe356e7dd0
Maciej Bąbol <maciekbbol@gmail.com>
2019-12-11 11:49:27 +0100

    Add anty forgery token for movies

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -54,6 +54,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Movie movie)
         {
             if (movie.Id == 0)
