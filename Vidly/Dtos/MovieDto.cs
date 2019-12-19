c596eb1427199bd3a58fcf74c3f73210a7f63b70
Maciej Bąbol <maciekbbol@gmail.com>
2019-12-19 13:01:23 +0100

    Building restful services (#6)
    * Add CustomerController API with http methods
    
    * Add Dto for customers
    
    * Add mapper for customer and customerDto
    
    * Add mapper for customer and customerDto #2
    
    * Camel case for Json
    
    * Add CRUD for movie
    
    * Small fixes for movies and customer id in mapping profile

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        
+        public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        [Range(1, 20, ErrorMessage = "The field Number in Stock must be between 1 and 20")]
+        public byte NumberInStock { get; set; }
+        [Required]
+        public byte GenreId { get; set; }
+    }
+}
